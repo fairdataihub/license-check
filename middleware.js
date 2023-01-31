ca08c9105f01a9ce0734936fbeb7bea4e90a6ec2
slugb0t <wheresdorian@gmail.com>
2023-01-31 15:58:59 -0800

    test: final try for url redirects

--- middleware.js
+++ middleware.js
@@ -3,15 +3,17 @@ import { rewrite } from '@vercel/edge';
 export function middleware(request) {
   console.log("Middleware activated");
   const url = new URL(request.url);
-
   console.log("URL is: " + url);
-  if (url.pathname.includes('privacy')) {
+
+  if (url.pathname.startsWith('/privacy')) {
     console.log("privacy page");
     return rewrite(new URL('/privacy.html', request.url));
   }
 
-  if (url.pathname.includes('support')) {
+  if (url.pathname.startsWith('/support')) {
     console.log("support page");
     return rewrite(new URL('/support.html', request.url));
   }
+
+  return;
 }
