b26d0955ea5ca63854485365318968374124b3e0
slugb0t <wheresdorian@gmail.com>
2023-01-31 15:55:26 -0800

    refactor: updated edge middleware

--- middleware.js
+++ middleware.js
@@ -1,16 +1,17 @@
 import { rewrite } from '@vercel/edge';
 
 export function middleware(request) {
+  console.log("Middleware activated");
   const url = new URL(request.url);
 
   console.log("URL is: " + url);
-  if (url.pathname.startsWith('/privacy')) {
+  if (url.pathname.includes('privacy')) {
     console.log("privacy page");
-    return rewrite(new URL('/privacy', request.url));
+    return rewrite(new URL('/privacy.html', request.url));
   }
 
-  if (url.pathname.startsWith('/support')) {
+  if (url.pathname.includes('support')) {
     console.log("support page");
-    return rewrite(new URL('/support', request.url));
+    return rewrite(new URL('/support.html', request.url));
   }
 }
